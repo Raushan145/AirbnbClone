d2243f54986c8b4f296e26bcd4b1ac716834f7d8
Raushan Kumar <raushankumar89780@gmail.com>
2026-07-03 16:42:04 +0530

    Configure MongoDB Atlas for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+      console.log(process.env.MONGODB_URL);
     await mongoose.connect(process.env.MONGODB_URL);
 
     console.log("✅ Database Connected");
