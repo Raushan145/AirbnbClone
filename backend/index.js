509ee9939a85695b67b87babd46b844cba83f600
Raushan Kumar <raushankumar89780@gmail.com>
2026-07-03 21:35:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 8080;
 app.use(cors({
     origin:"https://airbnbclone-b7rb.onrender.com",
     credentials:true
-// }))
+}))
 // app.use(cors({
 //   origin: [
 //     "http://localhost:5173",
